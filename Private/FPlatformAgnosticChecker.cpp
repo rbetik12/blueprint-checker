0f3426a8200da1a1bfb041251c913e643479d3c9
rbetik12 <mr.prikota@gmail.com>
2021-07-27 13:12:11 +0300

    Added blueprint file copying from user-provided directory to engine content directory

--- Private/FPlatformAgnosticChecker.cpp
+++ Private/FPlatformAgnosticChecker.cpp
@@ -1,20 +1,35 @@
 ﻿#include "FPlatformAgnosticChecker.h"
 #include "Containers/UnrealString.h"
 #include "Misc/Paths.h"
+#include "HAL/FileManager.h"
 #include <iostream>
 #include "FEngineWorker.h"
 
 void FPlatformAgnosticChecker::Check(const TCHAR* BlueprintPath)
 {
 	FEngineWorker::Init();
-	CopyFileToContentDir(BlueprintPath);
+	if (CopyFileToContentDir(BlueprintPath))
+	{
+		// TODO Blueprint parsing
+	}
 	FEngineWorker::Exit();
 }
 
-void FPlatformAgnosticChecker::CopyFileToContentDir(const TCHAR* BlueprintPath)
+bool FPlatformAgnosticChecker::CopyFileToContentDir(const TCHAR* BlueprintPath)
 {
-	const FString EngineContentDirPath(FPaths::EngineContentDir() + "BCTemp");
+	const FString EngineContentDirPath(FPaths::EngineContentDir() + "_Temp/_");
+	const FString DestFilePath = EngineContentDirPath + FPaths::GetCleanFilename(BlueprintPath);
+	
+	IFileManager* FileManager = &IFileManager::Get();
+	const uint32 CopyResult = FileManager->Copy(*DestFilePath, BlueprintPath, true);
 
-	std::wcout << (ToCStr(EngineContentDirPath)) << std::endl;
+	if (CopyResult != COPY_OK)
+	{
+		std::wcerr << "Can't copy file from " << BlueprintPath << " to " << *DestFilePath << std::endl;
+		return false;
+	}
+
+	std::wcout << "Successfully copied file from " << BlueprintPath << " to " << *DestFilePath << std::endl;
+	return true;
 }
 
--- Private/FPlatformAgnosticChecker.h
+++ Private/FPlatformAgnosticChecker.h
@@ -7,5 +7,5 @@ public:
 	// User provides absolute path to blueprint and we must convert it to engine-friendly path
 	static inline void Check(const TCHAR* BlueprintPath);
 private:
-	static inline void CopyFileToContentDir(const TCHAR* BlueprintPath);
+	static inline bool CopyFileToContentDir(const TCHAR* BlueprintPath);
 };
